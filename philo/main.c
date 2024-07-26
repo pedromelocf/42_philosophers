da4b05d5cae692f434fc8ffabaf63273ef782177
Pedro Melo <pedromelocf@yahoo.com>
2024-07-25 22:14:14 -0400

    Having some fun testing mutexs and ptreads, and inserting functions to initialize structure

--- philo/main.c
+++ philo/main.c
@@ -1,43 +1,113 @@
 #include "philosophers.h"
 
-static void     init_philo(t_philo **philo, int argc, char **argv);
+static void    init_diner(t_diner **diner, int argc, char **argv);
 static int      ft_atoi(const char *nptr);
 static void 	handle_exit(char *str, int status);
+static void		*philo_routine(void *arg);
+static void		*supervisor_routine(void *arg);
+
 
 int main(int argc, char **argv)
 {
-    t_philo    *philo;
+    t_diner    *diner;
 
-    init_philo(&philo, argc, argv);
-	printf(THINKING, philo->data->nb_philos, philo->philo_id);
-	//philo = philo->next;
+    init_diner(&diner, argc, argv);
     return (0);
 }
 
-static void    init_philo(t_philo **philo, int argc, char **argv)
+static void    init_diner(t_diner **diner, int argc, char **argv)
 {
-	int		i;
-	t_data	*data;
+    int i = 0;
 
-	i = 0;
-    if(argc != 5 && argc != 6)
+	if(argc != 5 && argc != 6)
 		handle_exit(EXPECTED_ARGS, 1);
-	data = &(t_data){
-			ft_atoi(argv[1]), ft_atoi(argv[2]),ft_atoi(argv[3]),
-			ft_atoi(argv[4]), 0
-	};
+    *diner = calloc(1, sizeof(t_diner));
+    (*diner)->data = calloc(1, sizeof(t_data));
+    (*diner)->data->nb_philos = ft_atoi(argv[1]);
+    (*diner)->data->time_to_die = ft_atoi(argv[2]);
+    (*diner)->data->time_to_eat = ft_atoi(argv[3]);
+    (*diner)->data->time_to_sleep = ft_atoi(argv[4]);
+    (*diner)->data->nb_meals_todo = -1;
 	if (argc == 6)
-		data->nb_meals_todo = ft_atoi(argv[5]);
-	while (i++ < data->nb_philos)
-	{
-		(*philo) = malloc(sizeof(t_philo));
-		memset((*philo), 0, sizeof(t_philo));
-		(*philo)->data = data;
-		(*philo)->nb_meals_done = 0;
-		(*philo)->philo_id = (short int)i;
-		(*philo) = (*philo)->next;
-	}
+		(*diner)->data->nb_meals_todo = ft_atoi(argv[5]);
+    (*diner)->supervisor = calloc(1, sizeof(t_supervisor));
+    (*diner)->supervisor->alive = TRUE;
+    (*diner)->supervisor->dining_info = *diner;
+    (*diner)->philos = calloc(1, sizeof(t_philos) * (*diner)->data->nb_philos);
+    (*diner)->fork = calloc(1, sizeof(t_fork) * (*diner)->data->nb_philos);
+    while (i < (*diner)->data->nb_philos)
+    {
+        (*diner)->philos[i].alive = TRUE;
+        (*diner)->philos[i].philo_id = i + 1;
+        (*diner)->philos[i].time_since_beggin_last_meal = 0;
+        (*diner)->philos[i].nb_meals_done = 0;
+        (*diner)->philos[i].left_fork = &(*diner)->fork[i + 1];
+        (*diner)->philos[i].right_fork = &(*diner)->fork[i + 2 % (*diner)->data->nb_philos];
+        pthread_mutex_init(&(*diner)->fork[i].fork_mutex, NULL);
+        (*diner)->fork[i].status = NOT_IN_USE;
+        i++;
+    }
 }
+
+//
+// int main(int argc, char **argv)
+// {
+// 	t_test *test;
+// 	pthread_t thread1;
+// 	pthread_t thread2;
+// 	pthread_t thread3;
+//
+// 	test =  malloc(sizeof(t_test));
+// 	test->i = 0;
+// 	test->stop = 0;
+// 	pthread_mutex_init(&test->mutex, NULL);
+// 	pthread_create(&thread1, NULL, &philo_routine, test);
+// 	pthread_create(&thread2, NULL, &philo_routine, test);
+// 	pthread_create(&thread3, NULL, &supervisor_routine, test);
+// 	pthread_join(thread1, NULL);
+// 	pthread_join(thread2, NULL);
+// 	pthread_join(thread3, NULL);
+// 	pthread_mutex_destroy(&test->mutex);
+// 	return (0);
+// }
+//
+// static void	*philo_routine(void *arg)
+// {
+// 	t_test *test = (t_test *)arg;
+//
+// 	while (test->i < 500000)
+// 	{
+// 		pthread_mutex_lock(&test->mutex);
+// 		if(test->stop == 1)
+// 		{
+// 			pthread_mutex_unlock(&test->mutex);
+// 			break;
+// 		}
+// 		printf("%d\n", test->i);
+// 		test->i++;
+// 		pthread_mutex_unlock(&test->mutex);
+// 		usleep(1);
+// 	}
+// 	return (NULL);
+// }
+//
+// static void	*supervisor_routine(void *arg)
+// {
+// 	t_test *test = (t_test *)arg;
+// 	while (1)
+// 	{
+// 		pthread_mutex_lock(&test->mutex);
+// 		if (test->i >= 34908)
+// 		{
+// 			printf("SUP!\n\n");
+// 			test->stop = 1;
+// 			pthread_mutex_unlock(&test->mutex);
+// 			break;
+// 		}
+// 		pthread_mutex_unlock(&test->mutex);
+// 	}
+// 	return(NULL);
+// }
 
 static void handle_exit(char *str, int status)
 {
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -2,6 +2,22 @@
 
 # define PHILO_H
 
+#ifndef TRUE
+# define TRUE 1
+#endif
+
+#ifndef FALSE
+# define FALSE 0
+#endif
+
+#ifndef IN_USE
+# define IN_USE 1
+#endif
+
+#ifndef NOT_IN_USE
+# define NOT_IN_USE 0
+#endif
+
 #ifndef EXPECTED_ARGS
 # define EXPECTED_ARGS "expected: ./philo number_of_philosophers time_to_die \
 time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n"
@@ -36,19 +52,48 @@ time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n"
 
 typedef struct s_data
 {
-    int			nb_philos;
-    int         time_to_die;
-    int         time_to_eat;
-    int         time_to_sleep;
-    int         nb_meals_todo;
+    short int		nb_philos;
+    int             time_to_die;
+    int             time_to_eat;
+    int             time_to_sleep;
+    int             nb_meals_todo;
 }    t_data;
 
-typedef struct s_philo
+// typedef struct s_test
+// {
+//     int i;
+//     int stop;
+//     pthread_mutex_t mutex;
+// }   t_test;
+
+typedef struct s_fork
 {
-    struct s_data   *data;
-    struct s_philo  *next;
+    short int       status;
+    pthread_mutex_t fork_mutex;
+}   t_fork;
+
+typedef struct s_philos
+{
+    t_fork          *left_fork;
+    t_fork          *right_fork;
     short int       philo_id;
+    short int       alive;
+    int             time_since_beggin_last_meal;
     int             nb_meals_done;
-}    t_philo;
+}    t_philos;
+
+typedef struct s_diner
+{
+    struct s_supervisor *supervisor;
+    struct s_philos     *philos;
+    struct s_data       *data;
+    struct s_fork       *fork;
+}   t_diner;
+
+typedef struct s_supervisor
+{
+    short int       alive;
+    t_diner         *dining_info;
+}  t_supervisor;
 
 #endif
