c08ba49c432e5a1b8417f4b4c9f01aa5726dd904
pmelo-ca <pedromelocf@yahoo.com>
2024-07-31 15:56:15 -0300

    Dealing with lone philo

--- philos.c
+++ philos.c
@@ -14,25 +14,36 @@
 
 void	taking_fork(t_philos *philos)
 {
-	printf(TAKEN_FORK, get_time_stamp() - philos->start_time, philos->philo_id);
+	if (philos->data->nb_philos == 1)
+	{
+		printf(TAKEN_FORK, get_time_stamp() - philos->start_time, philos->philo_id);
+		ft_usleep(philos->data->time_to_die + 1);
+	}
+//	printf(TAKEN_FORK, get_time_stamp() - philos->start_time, philos->philo_id);
 }
 
 void	eating(t_philos *philos)
 {
-	pthread_mutex_lock(&philos->last_meal->mutex);
-	philos->last_meal->state = get_time_stamp();
-	pthread_mutex_unlock(&philos->last_meal->mutex);
-	printf(EATING, get_time_stamp() - philos->start_time, philos->philo_id);
-	ft_usleep(philos->data->time_to_eat);
-	pthread_mutex_lock(&philos->nb_meals_done->mutex);
-	philos->nb_meals_done->state++;
-	pthread_mutex_unlock(&philos->nb_meals_done->mutex);
+	pthread_mutex_lock(&philos->philo_alive->mutex);
+	if (philos->philo_alive->alive == TRUE)
+	{
+		pthread_mutex_lock(&philos->last_meal->mutex);
+		philos->last_meal->state = get_time_stamp();
+		pthread_mutex_unlock(&philos->last_meal->mutex);
+		printf(EATING, get_time_stamp() - philos->start_time, philos->philo_id);
+		ft_usleep(philos->data->time_to_eat);
+		pthread_mutex_lock(&philos->nb_meals_done->mutex);
+		philos->nb_meals_done->state++;
+		pthread_mutex_unlock(&philos->nb_meals_done->mutex);
+	}
+	else
+		pthread_mutex_unlock(&philos->philo_alive->mutex);
 }
 
 void	sleeping(t_philos *philos)
 {
 	pthread_mutex_lock(&philos->philo_alive->mutex);
-	if (philos->philo_alive->alive != FALSE)
+	if (philos->philo_alive->alive == TRUE)
 	{
 		printf(SLEEPING, get_time_stamp() - philos->start_time, philos->philo_id);
 		pthread_mutex_unlock(&philos->philo_alive->mutex);
